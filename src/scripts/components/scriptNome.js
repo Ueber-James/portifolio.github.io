5ef1df06aa1709d649c5da3f321ed143b77c8c23
ueberjames <ueberjames@gmail.com>
2023-02-22 10:19:40 -0300

    test de deploy

--- src/scripts/components/scriptNome.js
+++ src/scripts/components/scriptNome.js
@@ -1,6 +1,6 @@
 export default function initTyping() {
     let letter = 0;
-    const text = 'daniel torres';
+    const text = 'Ueber James';
 
     function typeWriter() {
         if(letter < text.length) {
