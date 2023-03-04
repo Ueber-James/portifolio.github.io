c0895622c09861bccd52892f2dec186a70871ee8
ueberjames <ueberjames@gmail.com>
2023-03-04 17:59:04 -0300

    modificando scriptname para da a sensação de esta apagando e reescrevendo

--- src/scripts/components/scriptNome.js
+++ src/scripts/components/scriptNome.js
@@ -1,15 +1,45 @@
+// export default function initTyping() {
+//     let letter = 0;
+//     const text = 'Ueber James';
+
+//     function typeWriter() {
+//         if(letter < text.length) {
+//             document.querySelector(".meu-nome").innerHTML += text.charAt(letter);
+//             letter++;
+//             setTimeout(typeWriter, 180)
+//         }
+//     }
+
+//     typeWriter();
+
+// }
+
 export default function initTyping() {
     let letter = 0;
     const text = 'Ueber James';
+    let deleting = false;
 
     function typeWriter() {
-        if(letter < text.length) {
-            document.querySelector(".meu-nome").innerHTML += text.charAt(letter);
-            letter++;
-            setTimeout(typeWriter, 180)
-        }
+      const output = document.querySelector('.meu-nome');
+
+      if (deleting) {
+        output.textContent = text.substring(0, letter - 1);
+        letter--;
+      } else {
+        output.textContent = text.substring(0, letter + 1);
+        letter++;
+      }
+
+      if (letter === text.length + 1) {
+        deleting = true;
+        setTimeout(() => {
+          deleting = false;
+          letter = 0;
+        }, 3000);
+      }
+
+      setTimeout(typeWriter, 180);
     }
 
     typeWriter();
-
-}
+  }
